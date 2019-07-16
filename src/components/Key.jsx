0b982dd829ff8ced5e55ccf1f0ca54bf0b4c53ed
Fyum <fyulheri.dev@gmail.com>
2019-07-16 18:10:42 +0100

    add some animation

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 const StyledKey = styled.div`
   display: inline-block
@@ -25,6 +25,31 @@ const StyledEnharmonicKey = styled.div`
   top: 0
   margin-left: -15px
 `
+
+const height = keyframes`
+  from {
+    top: 0px;
+  }
+  to {
+    top: -500px ;
+  }
+`
+
+const heightAnimation = css`
+  ${height} 1.5s ease-out;
+`
+
+const StyledLine = styled.div`
+  width: 50px;
+  border-radius: 100%;
+  position: absolute;
+  top: 0px;
+  background-color: #8cf1f5;
+  height: 50px;
+  animation: ${heightAnimation};
+  animation-fill-mode: forwards;
+`
+
 const Key = ({
   id,
   name,
@@ -36,25 +61,43 @@ const Key = ({
 }) => {
   const [active, setActive] = useState(false);
 
+  
+  const [lines, setLines] = useState([]);
+
   const handleKeyDown = () => {
     setActive(true); 
     console.log({id}, {octave}, {name}, {number})
     playSound(octave, number);
-    setPressedKey(octave * 12 + number);
+    setPressedKey(`${name}-${octave}`);
+
+    setLines(lines.concat(1));
   }
 
   const handleKeyUp = () => {
     setActive(false);
+    if(lines.length > 5) { // Optimization so that it doesn't eat too much memory
+      setLines([]);
+    }
   }
 
   return (
     <>
+      
       <StyledKey
         // onMouseDown={handleKeyDown} // TODO find out why it is bugged
         onTouchStart={handleKeyDown}
         // onMouseUp={handleKeyUp}
         onTouchEnd={handleKeyUp}
       >
+        {
+        lines && 
+          lines.map(x => (
+            <StyledLine
+              active={active}
+            >
+            </StyledLine>
+          ))
+        }
         {
           type === 'NATURAL'
             ? <StyledNaturalKey active={active}>
